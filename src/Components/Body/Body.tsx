6218dbc09ebda32e5f19f251ea06ff4edf63e99b
Jpta24 <jeanpierretorres@gmail.com>
2021-06-22 00:23:46 +0200

    Projects Ready

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Hero';
 import Skills from './Skills';
 import AboutMe from './AboutMe';
+import Projects from './Projects';
 
 const Body = () => {
 	return (
@@ -9,6 +10,7 @@ const Body = () => {
 			<Hero />
 			<AboutMe />
 			<Skills />
+			<Projects />
 		</div>
 	);
 };
