3335a90d5c05f3a952c1ad91b573d1331d0219c0
Jpta24 <jeanpierretorres@gmail.com>
2021-06-22 19:12:51 +0200

    Footer ready

---
+++ frontend/src/Components/Body/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='text-center'>
+			<p className='footer py-3'>
+				Copyright © 2021. @jpta24 <br />
+				All Rights Reserved
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
