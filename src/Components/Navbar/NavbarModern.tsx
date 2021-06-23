793b3052e399afdf7ffbc3f9df1644e90fe08dbc
Jpta24 <jeanpierretorres@gmail.com>
2021-06-23 14:36:27 +0200

    Style NavbarModern

--- src/Components/Navbar/NavbarModern.tsx
+++ src/Components/Navbar/NavbarModern.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Container, Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 
 import logo from '../Body/Assets/coding_icon_color.png';
 
+interface NavbarInterface {
+	Home: string;
+	About: string;
+	Skills: string;
+	Portafolio: string;
+	Contact: string;
+}
+
 const NavbarModern = () => {
+	const initialState = {
+		Home: 'col-md-1 text-center mx-auto px-0',
+		About: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+		Skills: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+		Portafolio: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+		Contact: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+	};
+
+	const [Active, setActive] = useState<NavbarInterface>({
+		Home: 'col-md-1 text-center mx-auto px-0 active',
+		About: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+		Skills: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+		Portafolio: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+		Contact: 'col-md-2 text-center my-auto px-2 mx-2 NavBo',
+	});
+
+	const handleClick = (id: string) => {
+		if (id === 'Home') {
+			setActive({
+				...initialState,
+				[id]: 'col-md-1 text-center mx-auto px-0 activeB',
+			});
+		} else {
+			setActive({
+				...initialState,
+				[id]: 'col-md-2 text-center my-auto px-2 mx-2 NavBo activeN',
+			});
+		}
+	};
+
 	return (
 		<Navbar
 			variant='dark'
-			bg='dark'
 			collapseOnSelect
 			expand='md'
 			fixed='top'
-			className='py-1'
+			className='py-1 NavbarM'
 		>
 			<Container className='justify-content-end'>
 				<Navbar.Toggle
@@ -29,40 +66,52 @@ const NavbarModern = () => {
 						className='col-md-10 justify-content-center'
 					>
 						<Nav.Link
-							className='col-md-2 text-center my-auto px-2 mx-2 '
+							className={Active.About}
 							href='#aboutme'
+							onClick={() => {
+								handleClick('About');
+							}}
 						>
 							About Me
 						</Nav.Link>
 						<Nav.Link
-							className='col-md-2 text-center my-auto px-2 mx-2 '
+							className={Active.Skills}
 							href='#skills'
+							onClick={() => {
+								handleClick('Skills');
+							}}
 						>
 							Skills
 						</Nav.Link>
 
-						<Navbar.Brand
-							href='#hero'
-							className='col-md-1 mx-0 col-1 text-center brand'
-						>
+						<Navbar.Brand href='#hero' className={Active.Home}>
 							<img
 								src={logo}
 								width='60'
 								height='auto'
-								className='d-inline-block align-top'
+								className='d-inline-block align-top NavBrand'
 								alt='React Bootstrap logo'
+								onClick={() => {
+									handleClick('Home');
+								}}
 							/>
 						</Navbar.Brand>
 
 						<Nav.Link
-							className='col-md-2 text-center my-auto px-2 mx-2 '
+							className={Active.Portafolio}
 							href='#projects'
+							onClick={() => {
+								handleClick('Projects');
+							}}
 						>
 							Projects
 						</Nav.Link>
 						<Nav.Link
-							className='col-md-2 text-center my-auto px-2 mx-2 '
+							className={Active.Contact}
 							href='#contact'
+							onClick={() => {
+								handleClick('Contact');
+							}}
 						>
 							Contact
 						</Nav.Link>
